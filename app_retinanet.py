610e53126028af7fcd6b8a689ac0f0c003f95f60
miniBEar <ax_zaa@windowslive.com>
2019-06-30 17:10:46 +0700

    update: web with flex css

--- app_retinanet.py
+++ app_retinanet.py
@@ -147,5 +147,5 @@ def run(vdo_=0):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0', debug=True)
