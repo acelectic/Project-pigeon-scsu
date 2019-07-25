49f98e529ed7d1e686fdc91ac0e23a5ab7b624d7
miniBEar <ax_zaa@windowslive.com>
2019-07-26 03:00:24 +0700

    add time to elas_api

--- until/elas_api.py
+++ until/elas_api.py
@@ -21,14 +21,17 @@ class elas_api:
 
 
     def preSomething(self):
-        self.es_index = 'pigeon-test'
-        self.es_image = 'pigeon-image-test2'
+        # self.es_index = 'pigeon-test'
+        # self.es_image = 'pigeon-image-test2'
 
         # self.es_index = 'pigeon-recoed-test3'
         # self.es_image = 'pigeon-image-test3'
 
         # self.es_index = 'pre-data'
-        # self.es_image = 'pre-
+        # self.es_image = 'pre-'
+
+        self.es_index = 'test-index'
+        self.es_image = 'test-image'
 
         self.weekDays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
@@ -46,6 +49,26 @@ class elas_api:
             'max_retries': 0,
 
         }
+
+        self.labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus',
+                                6: 'train',
+                                7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign',
+                                12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse',
+                                18: 'sheep',
+                                19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack',
+                                25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis',
+                                31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat',
+                                35: 'baseball glove',
+                                36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass',
+                                41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple',
+                                48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza',
+                                54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed',
+                                60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote',
+                                66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster',
+                                71: 'sink',
+                                72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors',
+                                77: 'teddy bear',
+                                78: 'hair drier', 79: 'toothbrush'}
 
     def __connectElasNikko(self):
         config = self.config
@@ -89,7 +112,7 @@ class elas_api:
             body['timestamp'] = self.localtimezone(time_)
             # body['timestamp_utc'] = self.utctimezone(time_)
 
-            body['Hour_int'] = int(time_.strftime('%-H'))
+            body['Hour_int'] = self.hour_int(time_)
             body['Hour_text'] = self.hour_text[body['Hour_int']]
 
             body['dayofweek_int'] = self.dayofweek_int(time_)
@@ -102,7 +125,7 @@ class elas_api:
 
             body['processing_time'] = processing_time
             # print(body)
-            self.es.index(index=self.es_image, id=eventid, body=body)
+            self.es.index(index=self.es_image, doc_type="_doc", id=eventid, body=body)
 
     def elas_record(self, eventid, time_, label, score, box):
         if self.es_status:
@@ -110,7 +133,7 @@ class elas_api:
             body['timestamp'] = self.localtimezone(time_)
             # body['timestamp_utc'] = self.utctimezone(time_)
 
-            body['Hour_int'] = int(time_.strftime('%-H'))
+            body['Hour_int'] = self.hour_int(time_)
             body['Hour_text'] = self.hour_text[body['Hour_int']]
 
             body['dayofweek_int'] = self.dayofweek_int(time_)
@@ -125,7 +148,10 @@ class elas_api:
             body['box'] = {'x1': box[0], 'y1': box[1], 'x2': box[2], 'y2': box[3]}
 
             # print(body)
-            self.es.index(index=self.es_index, body=body)
+            self.es.index(index=self.es_index, doc_type="_doc", body=body)
+
+    def hour_int(self, time):
+        return time.hour
 
     def month_int(self, time):
         return time.month
