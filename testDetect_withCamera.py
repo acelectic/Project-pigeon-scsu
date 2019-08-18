f183864f46361e086f2517aeb54db9ee02193c0a
miniBEar <ax_zaa@windowslive.com>
2019-08-18 08:45:09 +0700

    test camera lock target

---
+++ testDetect_withCamera.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+from until.elas_api import elas_api
+es_ip = '192.168.1.29'
+es_port = 9200
+es = elas_api(ip=es_ip)
+es_status = None
+es_status = es.checkStatus()[0]
+
+from retinanet import testModel
+
+
+if __name__ == '__main__':
+    es = elas_api(ip = '192.168.1.29')
+    detect_model = testModel.Model(es=es, es_mode=True)
+
+    img = cv2.VideoCapture(0)
+    while 1:
+        _, frame = img.read()
+
+        if _:
+            detect_model.detect(frame)
+            time.sleep(10)
