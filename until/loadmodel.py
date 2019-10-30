63c394bc3f2f3e46db0273c5bec652cf1ec10029
miniBear <ax_zaa@windowslive.com>
2019-10-30 20:41:42 +0700

    add load data4eval

--- until/loadmodel.py
+++ until/loadmodel.py
@@ -1,6 +1,18 @@
 import os
 from keras.utils import get_file
 
+def load_data4eval():
+    
+    model_store = os.getcwd()
+    resnet_filename = 'data4eval.zip'
+    resnet_resource = 'https://github.com/acelectic/Project-pigeon-scsu/releases/download/v1.1/data4eval.zip'
+
+    get_file(
+        resnet_filename,
+        resnet_resource,
+        cache_subdir=model_store,
+    )
+
 
 def download_imagenet():
     """ Downloads ImageNet weights and returns path to weights file.
@@ -30,3 +42,4 @@ def download_imagenet():
 
 
 download_imagenet()
+load_data4eval()
