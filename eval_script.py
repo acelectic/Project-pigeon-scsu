f78aa4ada28e63f4535fa7437032ea01999cd820
miniBear <ax_zaa@windowslive.com>
2019-10-30 20:17:14 +0700

    add resnet101 script eval

--- eval_script.py
+++ eval_script.py
@@ -4,3 +4,4 @@ cmd_resnet50 = "retinanet-evaluate --backbone 'resnet50' --iou-threshold 0.5 --s
 cmd_resnet101 = "retinanet-evaluate --backbone 'resnet101' --iou-threshold 0.5 --score-threshold 0.5 --save-path 'evalresult/img-merge-resnet101/' --image-min-side 400 --image-max-side 400 csv data4eval/test_merge.csv data4eval/classes.txt models/resnet101/infer-resnet101.h5"
 
 os.system(cmd_resnet50)
+os.system(cmd_resnet101)
