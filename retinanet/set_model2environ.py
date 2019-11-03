19637b901e46b7a87f0846bba9be28088992a9b5
miniBear <ax_zaa@windowslive.com>
2019-11-04 03:45:29 +0700

    test with custom anchor

--- retinanet/set_model2environ.py
+++ retinanet/set_model2environ.py
@@ -5,3 +5,9 @@ print(os.environ['MODEL_RESNET50'])
 
 os.environ['MODEL_RESNET101'] = 'models/resnet101/infer-resnet101.h5'
 print(os.environ['MODEL_RESNET101'])
+
+os.environ['MODEL_cRESNET50'] = 'models/resnet50/infer-resnet50-cAnchor.h5'
+print(os.environ['MODEL_cRESNET50'])
+
+os.environ['MODEL_cRESNET101'] = 'models/resnet101/infer-resnet101-cAnchor.h5'
+print(os.environ['MODEL_cRESNET101'])
--- until/loadmodel.py
+++ until/loadmodel.py
@@ -38,6 +38,26 @@ def download_imagenet():
         cache_subdir=model_store,
     )
 
+    model_store = os.getcwd() + '/models/resnet50'
+    resnet_filename = 'infer-resnet50-cAnchor.h5'
+    resnet_resource = 'https://github.com/acelectic/Project-pigeon-scsu/releases/download/v1.2/model-infer-merge-resnet50-canchor-ep20-loss-0.1881.h5'
+
+    get_file(
+        resnet_filename,
+        resnet_resource,
+        cache_subdir=model_store,
+    )
+    
+    model_store = os.getcwd() + '/models/resnet101'
+    resnet_filename = 'infer-resnet101-cAnchor.h5'
+    resnet_resource = 'https://github.com/acelectic/Project-pigeon-scsu/releases/download/v1.2/model-infer-merge-resnet101-canchor-ep20-loss-0.3005.h5'
+
+    get_file(
+        resnet_filename,
+        resnet_resource,
+        cache_subdir=model_store,
+    )
+
     return
 
 
